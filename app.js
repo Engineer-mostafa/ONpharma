080e0acfdcd8b2794daa5ee9f31fe6488c1ac252
Mostafa Magdy <49917088+Engineer-mostafa@users.noreply.github.com>
2020-12-28 23:16:50 +0200

    HomeScreen Finished As Front And Forms

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 //engine
 const ejs = require('ejs');
 
-//to convert response into json format
+//to convert response into json format and give us access the form data
 const bodyParser = require('body-parser');
 
 // use the framework
@@ -19,20 +19,21 @@ const loginRoute = require('./routes/login');
 const medicalhistoryRoute = require('./routes/medicalhistory');
 const signupRoute = require('./routes/signup');
 const stockRoute = require('./routes/stock');
+const mainHallForPatients = require('./routes/mainHallForPatient');
 
 //local host
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 
-//default path
+//default path to access the files in the server not locally
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
-//virtual engine
+//virtual engine that allow us to write code in html files(views) like for loops
 app.set("view engine", 'ejs');
 
 //to convert response into json format
 app.use(bodyParser.json());
-//to able neasting object in json format
+//to able neasting object in json format this required explicitly we should write this if we need to use body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 
 //routes of the pages
@@ -41,12 +42,53 @@ app.use('/login', loginRoute);
 app.use('/signup', signupRoute);
 app.use('/medicalhistory', medicalhistoryRoute);
 app.use('/stock', stockRoute);
+app.use('/mainHallForPatient', mainHallForPatients);
 
 
 
 //server listen
-app.listen(port/*port*/, (error) => {
+app.listen(port, (error) => {
     if (error) return console.log(error);
     console.log(`listen at port ${port}`);
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//note for me <form action = ""  method = "post">
+//in forms action attribute i write on it the route i need this form to response in if i dont write any route by default the server will response in the same route
---
+++ routes/mainHallForPatient.js
@@ -0,0 +1,15 @@
+
+
+const router = require('express').Router();
+
+router.get('/', (request, Response) => {
+    Response.render('mainHallForPatient', {
+        title: "Main Hall",
+        css: "mainHallForPatient",
+        js: "mainHallForPatient"
+
+    }
+    );
+});
+
+module.exports = router;
