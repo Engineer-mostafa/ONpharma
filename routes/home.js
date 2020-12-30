bad5a849e253a1b7f4262146f0fdb37cae6f0cfa
Mostafa Magdy <49917088+Engineer-mostafa@users.noreply.github.com>
2020-12-30 07:04:48 +0200

    add forms

--- routes/home.js
+++ routes/home.js
@@ -12,4 +12,33 @@ router.get('/', (request, Response) => {
     );
 });
 
+
+router.post('/', (request, Response) => {
+    //type of the registiration tou will find 4 forms in views/home.ejs that represent all these types
+    switch (request.body.type) {
+        case "Patient":
+            console.log("Patient");
+            break;
+        case "Doctor":
+            console.log("Doctor");
+            break;
+
+        case "Pharmacist":
+            console.log("Pharmacist");
+            break;
+
+        default:
+            console.log("login");
+            break;
+    }
+    
+    Response.render('home',{
+        title: "Home",
+        css: "home",
+        js : "home"
+
+    }
+    );
+});
+
 module.exports = router;
