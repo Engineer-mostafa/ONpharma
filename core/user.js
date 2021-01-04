a40118e44c46309b68b8818a6474d5fcf53a862b
Yousif-Ahmed <yousif.lasheen60@gmail.com>
2021-01-04 04:35:19 +0200

    user validation and token for user

--- core/user.js
+++ core/user.js
@@ -1,155 +1,161 @@
 const pool = require('./Pool');
 const bcrypt = require('bcrypt');
-const { request } = require('express');
+const { request, response } = require('express');
 
 
-function User() {};
+function User() { };
 
 User.prototype = {
 
     // This function will search data in the database. 
     // Find the user data by account id or username.
 
-    find : function(user = null, callback)
-    {
+    find: function (user = null, callback) {
         // if the user variable is defind
-        if(user) {
+        if (user) {
             // if user = number return field = id, if user = string return field = username.
             var field = Number.isInteger(user) ? 'acc_ID' : 'acc_email';
         }
-        
-        // prepare the sql query
-        let sql_query ="SELECT * FROM account where acc_email = ? "  ;
 
-        
-        pool.query(sql_query,user,function(err, result) {
-            if(err) {throw err}
-           
-            if(result.length) {
-                callback(result);
-            }else {
-                callback(null);
+        // prepare the sql query
+        let sql_query = "SELECT * FROM account where acc_email = ? ";
+
+
+        pool.query(sql_query, user, function (err, result) {
+            if (err) { throw err }
+
+            else {
+                if (result.length > 0) {
+                    callback(result);
+                } else {
+                    callback(null);
+                }
             }
         });
     },
 
     // This function will insert data into the database. (create a new user)
     // body is an object 
-    create : function(body, callback) 
-    {
+    create: function (body, callback) {
 
         var pwd = body.password;
         var accmail = body.email;
-        
-        
+
+
         // Hash the password before insert it into the database.
-         body.password = bcrypt.hashSync(pwd,10);
-         
+        body.password = bcrypt.hashSync(pwd, 10);
+
         // this array will contain the values of the fields.
         var bind = [];
         // loop in the attributes of the object and push the values into the bind array.
-        for(prop in body){
-            
+        for (prop in body) {
+
             bind.push(body[prop]);
         }
         // validate 
-        this.find(accmail, function(USERFOUND) {
+        this.find(accmail, function (USERFOUND) {
             // if there is a user by this username
-            
-            try{
-                if(USERFOUND) {
-                // now we check his password.
-                throw 'email "' + USERFOUND[0].acc_email + '" is already taken';
+
+            try {
+                if (USERFOUND) {
+                    // now we check his password.
+                    response.status(400).send('Email already exits')
+                    // throw 'email "' + USERFOUND[0].acc_email + '" is already taken';
+                }
             }
-        }
-        catch{
-            console.log('Accepted your mail')
-        }
+            catch{
+                console.log('Accepted your mail')
+            }
         });
-        
+
         // prepare the sql query
         let sql = `INSERT INTO account (acc_email,acc_password,Fname,Mname,Lname,gender,Bdate,phoneNum) VALUES (?,?,?,?,?,?,?,?) `;
         // call the query give it the sql string and the values (bind array)
-        pool.query(sql, bind, async function(err, result) {
-            if(err) throw err;
-           
+        pool.query(sql, bind, async function (err, result) {
+            if (err) throw err;
+
             callback(result);
         });
+        return;
     },
 
-    login : function(user, password, callback)
-    {
-        
+    login: function (user, password, callback) {
+
         // find the user data by his username.
-        this.find(user, function(userID) {
+        this.find(user, function (userID) {
             // if there is a user by this username.
-       
-            if(userID) {
+
+            if (userID) {
                 // now we check his password.
-                if(bcrypt.compareSync(password, userID[0].acc_password)) {
+                if (bcrypt.compareSync(password, userID[0].acc_password)) {
                     // return his data.
                     callback(userID[0]);
                     return;
-                }  
+                }
             }
-            else           
-             // if the username/password is wrong then return null.
-            callback(null);
-        });
-        
-    },
-    Createpatient : function(user )
-    {
-        // getting user id 
-                var ID =user.insertId 
-                let sql = `INSERT INTO Patient (Patient_acc_ID) VALUES (?) `;
-               
-                // call the query give it the sql string and the value (user id )
-                pool.query(sql, ID, async function(err, result) {
-                    if(err) throw err;
-                    // return the last inserted id. if there is no error
-                    console.log('inserted Patient successfully....');
+            else
+                // if the username/password is wrong then return null.
+                {
+                    callback(null);
                     return ;
-                    
-                });          
+                }
+
+                
+        });
+
+    },
+    Createpatient: function (user) {
+        // getting user id 
+        var ID = user.insertId
+        let sql = `INSERT INTO Patient (Patient_acc_ID) VALUES (?) `;
+
+        // call the query give it the sql string and the value (user id )
+        pool.query(sql, ID, async function (err, result) {
+            if (err) throw err;
+            // return the last inserted id. if there is no error
+            console.log('inserted Patient successfully....');
+            return;
+
+        });
     },
     ////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////
     /////////////////should update for data base //////////////////
     //////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////
-     CreateDoctor : function(user )
-    {     
-          if(user) {
-                var ID =user.insertId
-                let sql = `INSERT INTO Doctor (doctor_acc_ID) VALUES ( ?)`;
-                // call the query give it the sql string and the values (bind array)
-                pool.query(sql, ID, async function(err, result) {
-                    if(err) throw err;
-                   
-                    console.log('inserted Doctor successfully....');
-                    return ;
-                });
-                
-            }
+    CreateDoctor: function (user) {
+        if (user) {
+            var ID = user.insertId
+            let sql = `INSERT INTO Doctor (doctor_acc_ID) VALUES ( ?)`;
+            // call the query give it the sql string and the values (bind array)
+            pool.query(sql, ID, async function (err, result) {
+                if (err) throw err;
+
+                console.log('inserted Doctor successfully....');
+               
+            });
+
+        }
+        return;
     },
-   
-    CreatePharmacist : function(user )
-    {
-        if(user) {
-                var ID =user.insertId
-                let sql = `INSERT INTO Pharmacist (Pharmacist_acc_ID) VALUES ( ?)`;
-                // call the query give it the sql string and the values (bind array)
-                pool.query(sql, ID, async function(err, result) {
-                    if(err) throw err;
-                    // return the last inserted id. if there is no error
-                    console.log('inserted pharmacist successfully....');
-                });
-                
-            }
-           
+
+    CreatePharmacist: function (user) {
+        if (user) {
+            var ID = user.insertId
+            let sql = `INSERT INTO Pharmacist (Pharmacist_acc_ID) VALUES ( ?)`;
+            // call the query give it the sql string and the values (bind array)
+            pool.query(sql, ID, async function (err, result) {
+                if (err) throw err;
+                // return the last inserted id. if there is no error
+                console.log('inserted pharmacist successfully....');
+            });
+
+
+        }
+        return ;
+
     }
-        
+
 
 }
 
--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,22 @@
 
+const jwt = require('jsonwebtoken');
+
 
 const router = require('express').Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../core/user');
-const { response } = require('express');
+
+const { response, request } = require('express');
+
+//FOR OUR VALIDATION
+const Joi = require('@hapi/joi');
+//schema for validation 
 
 const user = new User();
-
 router.get('/', (request, Response) => {
 
 
-    
+
     Response.render('home', {
         title: "Home",
         css: "home",
@@ -19,23 +25,31 @@ router.get('/', (request, Response) => {
     }
     );
 });
-
+const schema = Joi.object({
+    email: Joi.string().required().email(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    firstname: Joi.string().min(3).required(),
+    Minname: Joi.string().min(3).required(),
+    Lastname: Joi.string().min(3).required(),
+    gender: Joi.number().integer().required(),
+    Birthdate: Joi.string().required(),
+    phoneNum: Joi.number().integer().min(10).required()
+}
+)
 
 router.post('/', (request, Response) => {
-    
+
     var type = request.body.type;
 
     // for signup authontucation 
     if (type == "Patient" || type == "Doctor" || type == "Pharmacist") {
         //getting user information from user
-        var  gender =-1
-        if (request.body.Genderp)
-        {
-            gender =1
+        var gender = -1
+        if (request.body.Genderp) {
+            gender = 1
         }
-        else
-        {
-            gender =0 ;
+        else {
+            gender = 0;
         }
 
         var s = request.body.Bdate;
@@ -46,49 +60,75 @@ router.post('/', (request, Response) => {
             Minname: request.body.MNamep,
             Lastname: request.body.LastNamep,
             gender: gender,
-            Birthdate:  s ,
+            Birthdate: s,
             phoneNum: request.body.Mobilep
 
         };
-        user.create(userInput,async function(lastID) {
-            if (lastID) {
-    
+        // VALIDATE DATA BEFORE MAKING A USER
+       
+            const {error} = schema.validate(userInput);
+            //IF there is error then we don't create user  
+            if (error)
+            { 
+                //respose
+           return console.log(error.details[0].message) ;
+            }
+        
+        
+
+        user.create(userInput, async function (lastID) {
+
+            try {
+                if (lastID) {
+                     
                     switch (request.body.type) {
                         case "Patient":
-                            user.Createpatient(lastID);
+                           user.Createpatient(lastID);
                             break;
                         case "Doctor":
-                            user.CreateDoctor(lastID);
+                           user.CreateDoctor(lastID);
                             break;
 
                         case "Pharmacist":
-                            user.CreatePharmacist(lastID);
+                           user.CreatePharmacist(lastID);
                             break;
                     }
-                   //  redirect the user to the main page.
-                     Response.redirect('/mainHallForPatient');
+                    //  redirect the user to the main page.
+                    Response.redirect('/medicalhistory');
 
+
+                }
 
             }
-            
-            else {
-                console.log('Error creating a new user ....');
-            };
+            catch {
+                response.status(400).send(error);
+                
 
+            }
 
         })
-    }else {
+    } else {
         user.login(request.body.Emaill, request.body.Passwordl, function (result) {
-           
+
             if (result) {
-               
-              // console.log(result);
-                console.log("Sucssefully .....Login "+ result.acc_email);
+
+                // console.log(result);
+                console.log("Sucssefully .....Login " + result.acc_email);
+
+                console.log(result.acc_ID);
+                // CREATE and assign a token
+                const token = jwt.sign({_id:result.acc_ID},"secretkey");
+                console.log(token);
+                
+                // response.setheader({"Cookies":token});
                 // redirect the user to the home page.
-                Response.redirect('/mainHallForPatient');
+                 // response.redirect('/medicalhistory');
+                  
+              
+                
             } else {
 
-                Response.send('Username or Password incorrect!');
+               return  Response.status(400).send('Username or Password incorrect!');
             }
         })
     }
