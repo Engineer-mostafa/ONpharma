2cba50d63fbd86cdff64c139430975ac1b2f59af
Mostafa-wael <56788883+Mostafa-wael@users.noreply.github.com>
2021-01-09 20:15:57 +0200

    Data in table Doctor

--- OTHER/Schema/Schema_v_2_0.sql
+++ OTHER/Schema/Schema_v_2_0.sql
@@ -169,33 +169,54 @@ CREATE TABLE Chronic_Disease
     FOREIGN KEY (Patient_acc_ID) REFERENCES Account(acc_ID)
 );
 /********************************************************************/
+/*Account*/ 
+INSERT INTO `Account` (`acc_ID`,`acc_email`,`acc_password`,`Fname`,`Mname`,`Lname`,`gender`,`Bdate`,`phoneNum`,`User_type`)
+VALUES 
+(1,"Cras.pellentesque@Duisat.co.uk","HXX18WRZ6IY","Jeanette","Bradley","Upton","1","2000-10-20","01141471252","doctor"),(2,"tincidunt@malesuadaut.co.uk","GCZ60STJ0WJ","Steven","Armand","Austin","0","2012-10-15","01145192746","doctor"),
+(3,"Donec.tempus.lorem@consectetuer.edu","ZMX61UCS6KN","Medge","Zane","Felix","0","1990-07-05","01144839760","pharmacist"),(4,"convallis.dolor@sed.edu","CNS57ICQ6QG","Georgia","Maxwell","Levi","0","2000-10-20","01147281337","pharmacist"),
+(5,"turpis.Nulla@dolordapibus.net","ODQ95WKN8WF","Colorado","Nicholas","Owen","1","2012-10-15","01143432303","doctor"),(6,"Aliquam.erat@nequevenenatislacus.org","USR81MZS8RG","Levi","Kane","Reuben","1","1990-07-05","01140928218","doctor"),
+(7,"sed@nequeNullam.com","WZX31FRO8UU","Chantale","Rashad","Oliver","0","2000-10-20","01143262171","pharmacist"),(8,"neque@semper.ca","ISI16KIE4RY","Ferris","Octavius","Orlando","1","2012-10-15","01147867519","pharmacist"),
+(9,"non.sapien@quisurna.ca","BRS82LVI6QR","Knox","Arsenio","Thaddeus","0","1990-07-05","01146557968","doctor"),(10,"neque.Nullam.nisl@liberoProinmi.co.uk","KAD75BSC5NT","Ori","Bevis","Grady","1","2000-10-20","01149554281","doctor");
+INSERT INTO `Account` (`acc_ID`,`acc_email`,`acc_password`,`Fname`,`Mname`,`Lname`,`gender`,`Bdate`,`phoneNum`,`User_type`)
+VALUES
+(11,"felis@mattis.co.uk","CQH55PPZ7EF","Tanya","Wing","Barry","1","2012-10-15","01140350883","pharmacist"),(12,"quam@cursusinhendrerit.net","ZQF95PRY6OD","Lysandra","Baxter","Flynn","1","1990-07-05","01140494902","pharmacist"),
+(13,"aliquam@euplacerat.edu","INZ89NQB1GW","Chava","Erasmus","Yuli","1","2000-10-20","01146798042","doctor"),(14,"ipsum.Suspendisse@elementumloremut.ca","CYQ88LRY0ZP","Geraldine","Louis","Gannon","1","2012-10-15","01141705158","doctor"),
+(15,"malesuada.ut@faucibusleo.com","YXH26CTL4CV","Ali","Demetrius","Kenneth","1","1990-07-05","01141407102","pharmacist"),(16,"porttitor.eros@placerat.edu","KHF97CQJ9ZX","Julie","Todd","Brennan","1","2000-10-20","01149980737","pharmacist"),
+(17,"urna@magnatellus.net","XEW35LYA0UA","Hayfa","Abdul","Walker","0","2012-10-15","01146789606","doctor"),(18,"mollis.Integer@Lorem.net","TJY87WLS9PT","Talon","Magee","Baxter","1","1990-07-05","01145028532","doctor"),
+(19,"vitae.erat@at.edu","IJT43MJK0QN","Sydney","Moses","Demetrius","0","2000-10-20","01147634461","pharmacist"),(20,"neque@adipiscingelit.net","RAB18JMG2GI","Derek","Barclay","Francis","1","2012-10-15","01141507664","pharmacist");
+/*Doctor*/
+INSERT INTO `Doctor` (`doctor_acc_ID`,`doctor_degree`,`doctor_specialization`,`doctor_address`)
+VALUES
+(1,"Bachelor's Degree","Thoracic surgery","Ap #317-2414 Duis Rd."),(2,"Bachelor's Degree","Thoracic surgery","2139 Tincidunt Avenue"),
+(5,"Master's Degree","Radiation oncology","5029 Non, Rd."),(6,"Master's Degree","Radiation oncology","P.O. Box 232, 192 Fringilla St."),
+(9,"Doctoral Degree","Neurosurgery","Ap #985-7283 Placerat Avenue"),(10,"Bachelor's Degree","Thoracic surgery","Ap #249-718 Enim. Ave"),
+(13,"Bachelor's Degree","Thoracic surgery","P.O. Box 484, 6129 Cras St."),(14,"Master's Degree","Radiation oncology","9832 Sed St."),
+(17,"Doctoral Degree","Neurosurgery","5116 Varius. Avenue"),(18,"Doctoral Degree","Neurosurgery","3355 Elit, Avenue");
 /**/
-INSERT INTO `Account` (`acc_ID`,`acc_email`,`acc_password`,`Fname`,`Mname`,`Lname`,`gender`,`Bdate`,`phoneNum`,`User_type`) VALUES (1,"Cras.pellentesque@Duisat.co.uk","HXX18WRZ6IY","Jeanette","Bradley","Upton","1","2000-10-20","01141471252","doctor"),(2,"tincidunt@malesuadaut.co.uk","GCZ60STJ0WJ","Steven","Armand","Austin","0","2012-10-15","01145192746","doctor"),(3,"Donec.tempus.lorem@consectetuer.edu","ZMX61UCS6KN","Medge","Zane","Felix","0","1990-07-05","01144839760","pharmacist"),(4,"convallis.dolor@sed.edu","CNS57ICQ6QG","Georgia","Maxwell","Levi","0","2000-10-20","01147281337","pharmacist"),(5,"turpis.Nulla@dolordapibus.net","ODQ95WKN8WF","Colorado","Nicholas","Owen","1","2012-10-15","01143432303","doctor"),(6,"Aliquam.erat@nequevenenatislacus.org","USR81MZS8RG","Levi","Kane","Reuben","1","1990-07-05","01140928218","doctor"),(7,"sed@nequeNullam.com","WZX31FRO8UU","Chantale","Rashad","Oliver","0","2000-10-20","01143262171","pharmacist"),(8,"neque@semper.ca","ISI16KIE4RY","Ferris","Octavius","Orlando","1","2012-10-15","01147867519","pharmacist"),(9,"non.sapien@quisurna.ca","BRS82LVI6QR","Knox","Arsenio","Thaddeus","0","1990-07-05","01146557968","doctor"),(10,"neque.Nullam.nisl@liberoProinmi.co.uk","KAD75BSC5NT","Ori","Bevis","Grady","1","2000-10-20","01149554281","doctor");
-INSERT INTO `Account` (`acc_ID`,`acc_email`,`acc_password`,`Fname`,`Mname`,`Lname`,`gender`,`Bdate`,`phoneNum`,`User_type`) VALUES (11,"felis@mattis.co.uk","CQH55PPZ7EF","Tanya","Wing","Barry","1","2012-10-15","01140350883","pharmacist"),(12,"quam@cursusinhendrerit.net","ZQF95PRY6OD","Lysandra","Baxter","Flynn","1","1990-07-05","01140494902","pharmacist"),(13,"aliquam@euplacerat.edu","INZ89NQB1GW","Chava","Erasmus","Yuli","1","2000-10-20","01146798042","doctor"),(14,"ipsum.Suspendisse@elementumloremut.ca","CYQ88LRY0ZP","Geraldine","Louis","Gannon","1","2012-10-15","01141705158","doctor"),(15,"malesuada.ut@faucibusleo.com","YXH26CTL4CV","Ali","Demetrius","Kenneth","1","1990-07-05","01141407102","pharmacist"),(16,"porttitor.eros@placerat.edu","KHF97CQJ9ZX","Julie","Todd","Brennan","1","2000-10-20","01149980737","pharmacist"),(17,"urna@magnatellus.net","XEW35LYA0UA","Hayfa","Abdul","Walker","0","2012-10-15","01146789606","doctor"),(18,"mollis.Integer@Lorem.net","TJY87WLS9PT","Talon","Magee","Baxter","1","1990-07-05","01145028532","doctor"),(19,"vitae.erat@at.edu","IJT43MJK0QN","Sydney","Moses","Demetrius","0","2000-10-20","01147634461","pharmacist"),(20,"neque@adipiscingelit.net","RAB18JMG2GI","Derek","Barclay","Francis","1","2012-10-15","01141507664","pharmacist");
-/**/
+
+/*
 INSERT INTO `pharmacy_app_db`.`pharmaceutical_item` (`item_id_barcode`,`item_name`, `item_type`, `item_price`) VALUES (1,'bro', 'beauty', 10);
 INSERT INTO `pharmacy_app_db`.`pharmaceutical_item` (`item_id_barcode`,`item_name`, `item_type`, `item_price`) VALUES (2,'med', 'medicine', 12);
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`pharmacist` (`Pharmacist_acc_ID`) VALUES ('2');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`pharmacy` (`pharmacy_manager_ID`, `pharmacy_name`, `pharmacy_address`) VALUES (2, 'brother', 'shobra');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`pharmacy_repository` (`pharmacy_ID`, `item_id`, `item_quantity`) VALUES (1, 1, 50);
 INSERT INTO `pharmacy_app_db`.`pharmacy_repository` (`pharmacy_ID`, `item_id`, `item_quantity`) VALUES (1, 2, 100);
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`analysis` (`Analysis_Name`, `Analysis_Date`, `Result`, `Patient_acc_ID`) VALUES ('cbc', '2000-10-10', 'DB Lecture 3 - Fall 2020.pdf','1');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`chronic_disease` (`Disease_Name`, `Disease_Date`, `Patient_acc_ID`) VALUES ('brd', '2000-10-10', '1');
-/**/
-INSERT INTO `pharmacy_app_db`.`doctor` (`doctor_acc_ID`, `doctor_degree`, `doctor_specialization`, `doctor_address`) VALUES ('3', 'bech', 'suergery', 'shobra');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`prescription` (`Prescription_ID`, `Prescription_diagnosis`, `Prescription_date`, `Patient_acc_ID`, `doctor_acc_ID`) VALUES ('1', 'DB Lecture 1 - Fall 2020.pdf', '2000-10-10', '1', '3');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`prescription_medicines` (`Quantity`, `Item_id`, `Prescription_ID`) VALUES ('5', 1, '1');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`purchase_operation` (`operation_ID`, `pharmacy_ID`, `operation_cash`) VALUES ('1', '1', '10');
-/**/
+--
 INSERT INTO `pharmacy_app_db`.`scan` (`Scan_Name`, `Scan_Date`, `Result`, `Patient_acc_ID`) VALUES ('leg', '2000-10-10', 'DB Lecture 1 - Fall 2020.pdf', '1');
 INSERT INTO `pharmacy_app_db`.`scan` (`Scan_Name`, `Scan_Date`, `Result`, `Patient_acc_ID`) VALUES ('scan2', '2000-10-10', 'DB Lecture 6 - Fall 2020.pdf', '1');
-/**/
+--
+*/
 
