3aaf3e6652dbb763ca1a0ecaf0551e79d63306e3
Mostafa-wael <56788883+Mostafa-wael@users.noreply.github.com>
2021-01-10 04:37:07 +0200

    Crystal Report

---
+++ OTHER/Crystal Report/DBapplication/DBapplication/Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public class Controller
+    {
+        DBManager dbMan;
+
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+
+        public int InsertSupplier(string snum, string sname, string city, int status)
+        {
+            string query = "INSERT INTO S (S#, Name, City, Status) " +
+                            "Values ('" + snum + "','" + sname + "','" + city + "'," + status + ");";
+            return dbMan.ExecuteNonQuery(query);
+        }
+
+        public int DeleteSupplier(string snum)
+        {
+            string query = "DELETE FROM S WHERE S#='" + snum + "';";
+            return dbMan.ExecuteNonQuery(query);
+        }
+
+        public int UpdateSupplier(string snum, string city)
+        {
+            string query = "UPDATE S SET City='" + city + "' WHERE S#='" + snum + "';";
+            return dbMan.ExecuteNonQuery(query);
+        }
+
+        public DataTable SelectAllSuppliers()
+        {
+            string query = "SELECT * FROM S;";
+            return dbMan.ExecuteReader(query);
+        }
+
+        public int CountSuppliers()
+        {
+            string query = "SELECT COUNT(S#) FROM S;";
+            return (int)dbMan.ExecuteScalar(query);
+        }
+
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+    }
+}
---
+++ OTHER/Crystal Report/DBapplication/DBapplication/Report.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace DBapplication
+{
+    partial class Report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Report";
+            this.Text = "Report";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+    }
+}
