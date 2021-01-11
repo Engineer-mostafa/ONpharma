dea7b3380f835507400034466a872a6906ee5035
Mostafa Magdy <49917088+Engineer-mostafa@users.noreply.github.com>
2021-01-11 12:02:20 +0200

    home guides

--- public/js/home.js
+++ public/js/home.js
@@ -13,3 +13,18 @@ $('form input').keydown(function (e) {
 
 
 $('.alert').alert();
+
+
+    var enjoyhint_instance = new EnjoyHint({});
+
+var enjoyhint_script_steps = [{
+    'next .patienttour': 'if you are not a doctor or a pharmacist register from here'
+}, {
+    'click .guidetour': 'You also can read guides from here'
+}];
+
+
+enjoyhint_instance.set(enjoyhint_script_steps);
+
+enjoyhint_instance.run();
+
