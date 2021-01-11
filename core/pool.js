340cdb1bb9d52f67e4838e61b522424ff25e69d5
Mostafa Magdy <49917088+Engineer-mostafa@users.noreply.github.com>
2021-01-11 15:34:26 +0200

    c

--- core/pool.js
+++ core/pool.js
@@ -1,24 +1,24 @@
-const util = require('util');
-var mysql      = require('mysql');
+// const util = require('util');
+// var mysql      = require('mysql');
 
-const pool = mysql.createPool({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Mosstafalover999',
-  database : 'pharmacy_app_db'
-});
+// const pool = mysql.createPool({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'Mosstafalover999',
+//   database : 'pharmacy_app_db'
+// });
  
-pool.getConnection((err, connection)=>{
-    if (err) throw err ;
-    else
-    console.log("DB Connected to my sql"); 
-    if (connection)
-    connection.release() ;
-    return;
-});
+// pool.getConnection((err, connection)=>{
+//     if (err) throw err ;
+//     else
+//     console.log("DB Connected to my sql"); 
+//     if (connection)
+//     connection.release() ;
+//     return;
+// });
 
-  pool.query = util.promisify(pool.query);
-  module.exports=pool ;
+//   pool.query = util.promisify(pool.query);
+//   module.exports=pool ;
   
 
 
