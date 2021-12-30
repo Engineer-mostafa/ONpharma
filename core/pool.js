8a154cea3315741ffd8d49a69ccbf91e5814f09d
Mostafa Magdy <49917088+Engineer-mostafa@users.noreply.github.com>
2021-12-30 17:29:36 +0200

    break database to run

--- core/pool.js
+++ core/pool.js
@@ -1,22 +1,22 @@
-const util = require('util');
-var mysql      = require('mysql');
+// const util = require('util');
+// var mysql      = require('mysql');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE 
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER ,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DATABASE 
+// });
  
-pool.getConnection((err, connection)=>{
-    if (err) throw err ;
-    else
-    console.log("DB Connected to my sql"); 
-    if (connection)
-    connection.release() ;
-    return;
-});
+// pool.getConnection((err, connection)=>{
+//     if (err) throw err ;
+//     else
+//     console.log("DB Connected to my sql"); 
+//     if (connection)
+//     connection.release() ;
+//     return;
+// });
 
-  pool.query = util.promisify(pool.query);
-  module.exports=pool ;
+//   pool.query = util.promisify(pool.query);
+//   module.exports=pool ;
   
